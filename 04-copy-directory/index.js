97471f289f1dcad7c61fc0a9c14f73d7fe5ca61d
Vladislav Lekontsev <other2other2@gmail.com>
2023-05-05 22:32:45 +0400

    feat: complete task №4 copy dirctory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir } = require('fs/promises');
 
+const folderToCopy = path.join(__dirname, 'files');
+const folderAfterCopy = path.join(__dirname, 'files-copy');
+
+fs.mkdir(folderAfterCopy, { recursive: true }, (error) => {
+  if (error) throw error;
+});
+
+// ignore the different syntax of these two "readdir" i just try to use different ways to apply it
+fs.readdir(folderAfterCopy, (error, files) => {
+  if (error) throw error;
+
+  files.forEach(file => {
+    fs.unlink(path.join(folderAfterCopy, file), error => {
+      if (error) throw error;
+    })
+  });
+})
+
+readdir(folderToCopy)
+  .then((files) => {
+    files.forEach(file => {
+      fs.copyFile(
+        path.join(folderToCopy, file),
+        path.join(folderAfterCopy, file),
+        (err) => {
+          if (err) throw err;
+        });
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  })
